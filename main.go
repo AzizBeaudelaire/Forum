f539965e0ec7ad830eb96633e7b86ca0a91d5c6c
pjordan <jordan.palacios@ynov.com>
2023-06-19 17:18:36 +0200

    [ADD + PROBLEM]login register adapt to project

--- main.go
+++ main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
-	_ "github.com/mattn/go-sqlite3"
 )
 
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func main() {
-	db, err := sql.Open("sqlite3", "./data.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// Initialiser le routeur
 	r := mux.NewRouter()
-	fmt.Print("Écouter sur le port 8080...\n")
 
-	// Définir une route pour le fichier script.js
 	r.HandleFunc("/script.js", func(w http.ResponseWriter, r *http.Request) {
 		js, err := ioutil.ReadFile("script.js")
 		if err != nil {
@@ -33,6 +31,7 @@ func main() {
 		w.Header().Set("Content-Type", "text/javascript")
 		fmt.Fprint(w, string(js))
 	})
+
 	r.HandleFunc("/changement_pdp.js", func(w http.ResponseWriter, r *http.Request) {
 		js, err := ioutil.ReadFile("changement_pdp.js")
 		if err != nil {
@@ -42,6 +41,7 @@ func main() {
 		w.Header().Set("Content-Type", "text/javascript")
 		fmt.Fprint(w, string(js))
 	})
+
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			html, err := ioutil.ReadFile("./templates/html/login_page.html")
@@ -62,64 +62,64 @@ func main() {
 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 			fmt.Fprintf(w, "<html><head><title>Login Page</title><style>%s</style></head><body style=\"background-image: url('data:image/png;base64,%s')\">%s</body></html>", string(css), base64.StdEncoding.EncodeToString(image), string(html))
 		} else if r.Method == http.MethodPost {
-			file, _, err := r.FormFile("image")
+			err := r.ParseForm()
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			defer file.Close()
 
-			fmt.Fprint(w, "Photo de profil mise à jour avec succès")
+			username := r.Form.Get("username")
+			password := r.Form.Get("password")
+			email := r.Form.Get("email")
+
+			// Create a new User instance
+			user := User{
+				Username: username,
+				Email:    email,
+				Password: password,
+			}
+
+			// Read existing user data from JSON file
+			users, err := readUsersFromFile("users.json")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Add the new user to the existing users slice
+			users = append(users, user)
+
+			// Write the updated users data to the JSON file
+			err = writeUsersToFile(users, "users.json")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Redirect to a success page
+			http.Redirect(w, r, "/success", http.StatusSeeOther)
 		}
 	})
-	// Définir une route pour la page d'accueil
-	r.HandleFunc("./templates/html/login_page.html", func(w http.ResponseWriter, r *http.Request) {
-		html, err := ioutil.ReadFile("./templates/html/error404.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		css, err := ioutil.ReadFile("./templates/css/styleError.css")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		image, err := ioutil.ReadFile("static/images/404-error-page-examples-best.jpg")
+
+	r.HandleFunc("/success", func(w http.ResponseWriter, r *http.Request) {
+		html, err := ioutil.ReadFile("./templates/html/success.html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprintf(w, "<html><head><title>Error 404 page</title><style>%s</style></head><body style=\"background: url('data:image/jpg;base64,%s') no-repeat center center fixed; background-size: cover;\">%s</body></html>", string(css), base64.StdEncoding.EncodeToString(image), string(html))
-
+		fmt.Fprintf(w, "<html><head><title>Success Page</title></head>%s</body></html>", string(html))
 	})
-	// Définir une route pour récupérer les données de la base de données
+
 	r.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT * FROM users")
+		users, err := readUsersFromFile("users.json")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		defer rows.Close()
-
-		var (
-			id   int
-			name string
-		)
-
-		for rows.Next() {
-			err := rows.Scan(&id, &name)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			fmt.Fprintf(w, "ID: %d, Name: %s\n", id, name)
-		}
-
-		if err := rows.Err(); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+		for _, user := range users {
+			fmt.Fprintf(w, "Username: %s, Email: %s, Password: %s\n", user.Username, user.Email, user.Password)
 		}
 	})
 
@@ -128,4 +128,43 @@ func main() {
 
 	fmt.Printf("Server is running on http://localhost:8080/\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func readUsersFromFile(filename string) ([]User, error) {
+	filePath := filepath.Join("users.json")
+	file, err := os.Open(filePath)
+	if err != nil {
+		// If the file doesn't exist, return an empty slice of users
+		if os.IsNotExist(err) {
+			return []User{}, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	// Decode the JSON data into a slice of User
+	var users []User
+	err = json.NewDecoder(file).Decode(&users)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func writeUsersToFile(users []User, filename string) error {
+	filePath := filepath.Join("users.json")
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Encode the users slice as JSON and write it to the file
+	err = json.NewEncoder(file).Encode(users)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
